67e826ee6fa63f81af889db6c7ceecbb2f7d7cf5
exzhibit97 <exzhibit97@gmail.com>
2018-11-22 17:24:53 +0100

    another commit - updated (added files)

--- src/main/java/src/main/companions/MyDesign.java
+++ src/main/java/src/main/companions/MyDesign.java
@@ -1,5 +1,10 @@
 package src.main.companions;
 
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.polymertemplate.Id;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.router.Route;
 import com.vaadin.flow.templatemodel.TemplateModel;
 import com.vaadin.flow.component.Tag;
 import com.vaadin.flow.component.dependency.HtmlImport;
@@ -13,13 +18,24 @@ import com.vaadin.flow.component.polymertemplate.PolymerTemplate;
  */
 @Tag("my-design")
 @HtmlImport("my-design.html")
+@Route("mydesign")
 public class MyDesign extends PolymerTemplate<MyDesign.MyDesignModel> {
+
+    @Id("vaadinButton")
+    private Button vaadinButton;
+    @Id("vaadinButton1")
+    private Button vaadinButton1;
+    @Id("vaadinTextArea")
+    private TextArea vaadinTextArea;
 
     /**
      * Creates a new MyDesign.
      */
     public MyDesign() {
-        // You can initialise any data required for the connected UI components here.
+        vaadinButton.addClickListener(event -> {
+            Notification.show(vaadinTextArea.getValue(), 1000 * 10,
+                    Notification.Position.MIDDLE);
+        });
     }
 
     /**
